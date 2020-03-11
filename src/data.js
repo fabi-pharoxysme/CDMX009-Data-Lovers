d3529d8b4b1be6a6871e9d4295f507789fc99770
fabi-pharoxysme <fabio.ola9859@gmail.com>
2020-03-11 13:06:11 -0600

    Modal que abre y cierra

--- src/data.js
+++ src/data.js
@@ -2,22 +2,24 @@ import data from './data/pokemon/pokemon.js';
 // esta es una función de ejemplo
 //Mostrar la data
 export const dataPokemon= data.pokemon;
-  let nuevaListaFiltrada = data.pokemon.filter(p=>p.type.includes(/*value*/'Fire'))
-//}
+//Filtracion 
+function onChange(e){
+  let value = e.target.value
+  let nuevaListaFiltrada = data.pokemon.filter(p=>p.type.includes(value))
+}
+//console.log(nuevaListaFiltrada)
+  
 
-export const images = ()=> {
-  data.pokemon.forEach((item) => {
+/*export const images = data.pokemon.forEach((item) => {
     let image= item.img;
     let label= document.createElement('img');
     let btn= document.createElement('button');
-    btn.className= ("btn-Images")
+    btn.className+= ("btn-Images")
     btn.appendChild(label);
     label.src= image;
     let node= btn;
     document.getElementById("boxImages").appendChild(node);
-  })
-}
+  });*/
 
-console.log(nuevaListaFiltrada);
 
 // `<img src="${node}" alt="${item.name}>`
--- src/main.js
+++ src/main.js
@@ -1,14 +1,22 @@
-import {images, dataPokemon} from './data.js';
+import {dataPokemon} from './data.js';
 
-console.log(images());
-// const start= document.querySelector('.dinamic');
+console.log();
 //Nodos 
 let modal = document.querySelector(".modal-container");
-let flex = document.querySelector(".flex");
 let closeModal = document.querySelector(".close");
 let openModal = document.querySelector("#ranking"); 
-let openmodalPokemon = document.querySelector(".btn-Images");
 let modalPokemon = document.querySelector(".modalPokemon")
+  //Mostrar imagenes pantalla principal
+  const images = dataPokemon.forEach((item) => {
+    let image= item.img;
+    let label= document.createElement('img');
+    let btn= document.createElement('button');
+    btn.className+= ("btn-Images")
+    btn.appendChild(label);
+    label.src= image;
+    let node= btn;
+    document.getElementById("boxImages").appendChild(node);
+  });
 
     //Modal 
  openModal.addEventListener('click', function(){
@@ -17,13 +25,14 @@ let modalPokemon = document.querySelector(".modalPokemon")
 
 closeModal.addEventListener('click', function(){
     modal.style.display ='none';
-});
+})
 
-window.addEventListener('click',function(e){
+/*window.addEventListener('click',function(e){
     if (e.target == flex){
         modal.style.display= 'none';
     }
-});
+});/*
+
 
 /*dataPokemon.forEach(element => {
     let text = element.name;
@@ -37,7 +46,7 @@ const modalPokemons= dataPokemon.forEach(poke=>{
     <div class= " modal-header-pokemon">
     <h3 class="num"> ${poke.num}</h3>
     <h4 class= "name"> ${poke.name}</h4>
-    <span class= "close" id= "close"> &times; <span>
+    <span class= "closePokemon" id= "close"> &times; <span>
     </div>
     <div class= "modal-body-pokemon>
     <div class= "img-contain"> <img class="img poke" src='${poke.img}' /> </div>
@@ -53,5 +62,13 @@ const modalPokemons= dataPokemon.forEach(poke=>{
     <span class="next-evolution> ${poke.next_evolution}</span>
     </div>`
 });
- 
- 
+let closeModalPokemon = document.querySelector(".closePokemon");
+closeModalPokemon.addEventListener('click',function(){
+    modalPokemon.style.display = 'none';
+}); 
+
+let openModalPokemon = document.querySelector(".btn-Images")
+    openModalPokemon.addEventListener('click',function(){
+        modalPokemon.style.display = 'block'
+    });
+
