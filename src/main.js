d64b5ae3cb6364b9034650f23d0149f4966d849d
EdithOrt <eoc.diseno@gmail.com>
2020-03-20 08:33:03 -0600

    subiendo cambios modal sin poder jalar data UNDEFINED

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import {dataPokemon} from './data.js';
+import pokemon from './data/pokemon/pokemon.js';
 
 console.log();
 //Nodos 
@@ -18,7 +19,40 @@ const images = dataPokemon.forEach((item) => {
     label.src= image;
     let node= btn;
     document.getElementById("boxImages").appendChild(node);
+    
+
+
 });
+
+    let boxImages=document.querySelector("#dinamic");
+
+
+
+let btn= document.querySelectorAll('.btn-Images');
+btn.forEach (pokebola => 
+    pokebola.addEventListener('click',()=>{
+    document.getElementById("modalPokemon").style.display="block";
+    
+    
+    showData(dataPokemon).forEach(Element);    
+    function showData(poke){
+    let container= document.getElementById("contentInfo");
+let cardInfo= document.createElement('div');
+container.innerHTML=`
+        <h3 class="num"> ${poke.num}</h3>
+        <h4 class= "name"> ${poke.name}</h4>
+        <div class= "img-contain"> <img class="img poke" src='${poke.img}' /> </div>
+        <p class="type"> Tipo: ${poke.type} </p>
+        <p class="weigth"> Peso: ${poke.weight}  Altura: ${poke.height} </p>
+        <p class="candy"> Dulces: ${poke.candy_count} ${poke.candy}</p>
+        <p class="egg"> Tipo de huevo: ${poke.egg}</p>
+        <p class="average spawn"> Frecuencia de aparición: ${poke.avg_spawns}</p>
+        <h3 class= "evolutions"> Evoluciones </h3>
+        <p class="prev-evolution"> ${poke.prev_evolution}</p>
+        <span class="next-evolution> ${poke.next_evolution}</span>`;
+document.querySelector('#card').appendChild(cardInfo);
+}
+}))
 
     //Modal 
  /*openModal.addEventListener('click', function(){
@@ -40,39 +74,30 @@ closeModal.addEventListener('click', function(){
     let text = element.name;
     console.log(text);
    });*/
-   let select= document.querySelectorAll('.btn-Images');
+//    let select= document.querySelectorAll('.btn-Images');
 
-   select.forEach(pokebola=>{
-    pokebola.addEventListener('click', e=>printInfo(e));
-})
+//    select.forEach(pokebola=>{
+//     pokebola.addEventListener('click', e=>printInfo(e));
+// })
 
-   function printInfo(){ dataPokemon.forEach( poke => {
-    let nodo= document.createElement('div');
-    nodo.className+= ("nodo");
-    nodo.innerHTML += `
-    <div class= "contenido-modal-pokemon">
-    <div class= " modal-header-pokemon">
-    <h3 class="num"> ${poke.num}</h3>
-    <h4 class= "name"> ${poke.name}</h4>
-    <span class= "closePokemon" id= "close"> &times; <span>
-    </div>
-    <div class= "modal-body-pokemon>
-    <div class= "img-contain"> <img class="img poke" src='${poke.img}' /> </div>
-    <p class="type"> Tipo: ${poke.type} </p>
-    <p class="weigth"> Peso: ${poke.weight}  Altura: ${poke.height} </p>
-    <p class="candy"> Dulces: ${poke.candy_count} ${poke.candy}</p>
-    <p class="egg"> Tipo de huevo: ${poke.egg}</p>
-    <p class="average spawn"> Frecuencia de aparición: ${poke.avg_spawns}</p>
-    </div>
-    <div class= "modal-footer-pokemon">
-    <h3 class= "evolutions"> Evoluciones </h3>
-    <p class="prev-evolution"> ${poke.prev_evolution}</p>
-    <span class="next-evolution> ${poke.next_evolution}</span>
-    </div>`;
-    let modalPokemon = document.querySelector("#modalPokemon");
-    nodo.style.display="block";
-    modalPokemon.appendChild(nodo);
-});
+   function showModals(showData){
+       let info= '';
+       
+       function showData (poke){
+        info += `
+        <h3 class="num"> ${poke.num}</h3>
+        <h4 class= "name"> ${poke.name}</h4>
+        <div class= "img-contain"> <img class="img poke" src='${poke.img}' /> </div>
+        <p class="type"> Tipo: ${poke.type} </p>
+        <p class="weigth"> Peso: ${poke.weight}  Altura: ${poke.height} </p>
+        <p class="candy"> Dulces: ${poke.candy_count} ${poke.candy}</p>
+        <p class="egg"> Tipo de huevo: ${poke.egg}</p>
+        <p class="average spawn"> Frecuencia de aparición: ${poke.avg_spawns}</p>
+        <h3 class= "evolutions"> Evoluciones </h3>
+        <p class="prev-evolution"> ${poke.prev_evolution}</p>
+        <span class="next-evolution> ${poke.next_evolution}</span>`;
+       }
+    container.innerHTML=info;
 }
 
 
@@ -96,4 +121,3 @@ closeModal.addEventListener('click', function(){
 //         })
 //            })
 //      });
-    
