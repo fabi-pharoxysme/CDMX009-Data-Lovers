b5721d4d1d410e795b7ab8f3cbf4fe13e7c4f5ad
fabi-pharoxysme <fabio.ola9859@gmail.com>
2020-03-23 12:44:11 -0600

    Modal que abre y cierra con info correcta y comienzo de filtración

--- src/main.js
+++ src/main.js
@@ -3,12 +3,9 @@ import pokemon from './data/pokemon/pokemon.js';
 
 console.log();
 //Nodos 
-let modal = document.querySelector(".modal-container");
-let closeModal = document.querySelector(".close");
-let openModal = document.querySelector("#ranking");
+let closeModal = document.querySelectorAll(".close");
 
-
-  //Mostrar imagenes pantalla principal
+//Mostrar imagenes pantalla principal
 const images = dataPokemon.forEach((item) => {
      let image= item.img;
      let label= document.createElement('img');
@@ -21,28 +18,40 @@ const images = dataPokemon.forEach((item) => {
      let node= btn;
      document.getElementById("boxImages").appendChild(node);
   });
-   //Modal Información Pokemon   
+//Modal Información Pokemon   
 
-     let btnImg= document.querySelectorAll('.btn-Images');
+     let btnImg = document.querySelectorAll('.btn-Images');
+     let Imgs = btnImg.id
       btnImg.forEach(poke =>{ 
-        let a= poke.id -1;
-        console.log(a)
+        let a = poke.id -1;
         
-        
-
-        
-    
-         const showData = () => {
+            const showData = () => {
             
              document.getElementById("modalPokemon").style.display="block";
              let container= document.getElementById("contentInfo");
-             let cardInfo= document.createElement('div');
+             document.createElement('div');
              container.innerHTML=`
-             <h3 class="num"> ${dataPokemon[a].name}</h3>`
-             //  document.querySelector('#card').appendChild(cardInfo);
+             <h3 class="num"> ${dataPokemon[a].num}</h3>
+             <h4 class= "name"> ${dataPokemon[a].name}</h4>
+             <div class= "img-contain"> <img class="img poke" src='${dataPokemon[a].img}' /> </div>
+             <p class="type"> Tipo: ${dataPokemon[a].type} </p>
+             <p class="weigth"> Peso: ${dataPokemon[a].weight}  Altura: ${dataPokemon[a].height} </p>
+             <p class="candy"> Dulces: ${dataPokemon[a].candy_count} ${dataPokemon[a].candy}</p>
+             <p class="egg"> Tipo de huevo: ${dataPokemon[a].egg}</p>
+             <p class="average spawn"> Frecuencia de aparición: ${dataPokemon[a].avg_spawns}</p>`
             }
          poke.addEventListener('click',showData)          
         });                  
-       
-    // }); 
-          
+     closeModal.forEach(closeModalPoke=>{
+      closeModalPoke.addEventListener('click',function(){
+          document.getElementById("modalPokemon").style.display = 'none';})
+        }); 
+      
+ //Filtración  
+   let options= document.querySelector(".dropdown-content");
+    options.addEventListener('click',onChange)
+    function onChange(e){
+      let value = e.target.value
+      let nuevaListaFiltrada = dataPokemon.filter(p=>p.type.includes(value))
+     console.log(nuevaListaFiltrada)
+   };
