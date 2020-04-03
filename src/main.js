81d0c41a5675afb125caa026ca669dd4cead43d4
fabi-pharoxysme <fabio.ola9859@gmail.com>
2020-04-03 14:58:42 -0600

    modal pantalla principal

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import {dataPokemon, newList} from './data.js';
+import pokemon from './data/pokemon/pokemon.js';
 
 //Nodos 
 let closeModal = document.querySelectorAll(".close");
@@ -13,7 +14,7 @@ const images = (data) => {
     label.id= item.id
     let btn= document.createElement('button');
     btn.classList.add ("btn-Images")
-    btn.id+= item.id
+    btn.id+= (item.id -1);
     btn.appendChild(label);
     label.src= image;
     let node= btn;
@@ -24,36 +25,37 @@ const images = (data) => {
 images(dataPokemon);
 
 //Modal Información Pokemon   
-
-// let btnImg = document.querySelectorAll('.btn-Images');
-function printModal (pokemon) {
-  let btnImg = document.querySelectorAll('button');
-  btnImg.forEach(poke =>{ 
-  let a = poke.id -1
-        
-    const showData = () => {
-     
-      document.getElementById("modalPokemon").style.display="block";
-      let container= document.getElementById("contentInfo");
-      document.createElement('div');
-      container.innerHTML=`
-      <h3 class="num"> ${pokemon[a].num}</h3>
-      <h4 class= "name"> ${pokemon[a].name}</h4>
-      <div class= "img-contain"> <img class="img poke" src='${pokemon[a].img}' /> </div>
-      <p class="type"> Tipo: ${pokemon[a].type} </p>
-      <p class="weigth"> Peso: ${pokemon[a].weight}  Altura: ${pokemon[a].height} </p>
-      <p class="candy"> Dulces: ${pokemon[a].candy_count} ${pokemon[a].candy}</p>
-      <p class="egg"> Tipo de huevo: ${pokemon[a].egg}</p>
-      <p class="average spawn"> Frecuencia de aparición: ${pokemon[a].avg_spawns}</p>`
-    }
-    poke.addEventListener('click',showData)          
+function printModal () {
+  let btnImg = document.querySelectorAll('.btn-Images');
+  console.log(btnImg)
+  btnImg.forEach(btns =>{ 
+    btns.addEventListener('click',(event)=>{
+      let valueBtn = event.target.id -1;
+      console.log(modal(dataPokemon,valueBtn));
+    })    
   });                
   closeModal.forEach(closeModalPoke=>{
     closeModalPoke.addEventListener('click',function(){
-      document.getElementById("modalPokemon").style.display = 'none';})
-    }); 
-  };   
-  printModal(dataPokemon);
+      document.getElementById("modalPokemon").style.display = 'none';
+    })
+  }); 
+};   
+// printModal(modal(dataPokemon));
+const modal = (pokemon,btn) => {
+  document.getElementById("modalPokemon").style.display="block";
+  let container= document.getElementById("contentInfo");
+  document.createElement('div');
+  container.innerHTML=`
+  <h3 class="num"> ${pokemon[btn].num}</h3>
+  <h4 class= "name"> ${pokemon[btn].name}</h4>
+  <div class= "img-contain"> <img class="img poke" src='${pokemon[btn].img}' /> </div>
+  <p class="type"> Tipo: ${pokemon[btn].type} </p>
+  <p class="weigth"> Peso: ${pokemon[btn].weight}  Altura: ${pokemon.height} </p>
+  <p class="candy"> Dulces: ${pokemon[btn].candy_count} ${pokemon[btn].candy}</p>
+  <p class="egg"> Tipo de huevo: ${pokemon[btn].egg}</p>
+  <p class="average spawn"> Frecuencia de aparición: ${pokemon[btn].avg_spawns}</p>`
+};
+printModal(modal);
  //Filtración
 let options= document.querySelector(".dropdown-content");
 options.addEventListener('click', (e)=>{
@@ -61,5 +63,24 @@ options.addEventListener('click', (e)=>{
    let dataFilter = newList(dataPokemon,value)
    contentImages.innerHTML = '';
    images(dataFilter);
-   printModal(dataFilter);
+   function printModal () {
+    let btnImg = document.querySelectorAll('.btn-Images');
+    console.log(btnImg)
+    btnImg.forEach(btns =>{ 
+      btns.addEventListener('click', console.log('holi'));
+        // event)=>{
+      //   let valueBtn = event.target.id;
+      //   if (valueBtn == dataFilter.id){
+      //   console.log(modal(dataFilter,valueBtn));
+      // }
+      // } )    
+    });                
+    closeModal.forEach(closeModalPoke=>{
+      closeModalPoke.addEventListener('click',function(){
+        document.getElementById("modalPokemon").style.display = 'none';
+      })
+    }); 
+  };   
+  printModal(modal)
   });
+  
